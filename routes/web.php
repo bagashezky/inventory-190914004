8c2ee08924464ff61b5b288c8c61fd7737ad39eb
Bagas Pratama <bagashpratama95@Gmail.com>
2021-10-12 16:01:17 +0700

    Perubahan web.php

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,19 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/bagas', function () {
+    echo "Hello World";
+});
+
+Route::get('/name/{bagas}', function ($bagas) {
+    return "Hello " . $bagas;
+});
+
+Route::get('/name/{bagas}', function ($bagas) {
+    return "Hello " . $bagas;
+})->where('bagas', '[A-Za-z]+');
+
+Route::get('/name/{nrp}/{bagas}', function ($nrp,$bagas) {
+    return $nrp ." " . $bagas;
+});
